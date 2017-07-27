190a3bfaf90c8699c086b8c6f3ff6e808f718621
BlueSky-07 <admin@ihint.me>
2017-07-28 00:35:22 +0800

    upload

---
+++ dictsFunctions.js
@@ -0,0 +1,113 @@
+'use strict';
+
+function getDictByPair(dictId, pairs) {
+    if (!checkPairs(pairs)) {
+        return ''
+    } else {
+        if (pairs[0] === '') {
+            return getDictByTeuu(dictId, pairs[1]);
+        } else {
+            return [getDictByUgmu(dictId, pairs[0]), getDictByYpmu(dictId, pairs[1])]
+        }
+    }
+}
+
+function getDictByUgmu(dictId, key) {
+    var val = dicts[getDictsById[dictId]]['ugmu'][key];
+    return val === undefined ? '' : val;
+}
+
+function getDictByYpmu(dictId, key) {
+    var val = dicts[getDictsById[dictId]]['ypmu'][key];
+    return val === undefined ? '' : val;
+}
+
+function getDictByTeuu(dictId, key) {
+    var val = dicts[getDictsById[dictId]]['teuu'][key];
+    return val === undefined ? '' : val.split('');
+}
+
+function getRandomPair() {
+    var ugmu = allowUgmus[Math.floor(Math.random() * allowUgmus.length)];
+    var ypmu = allowPairs[ugmu][Math.floor(Math.random() * allowPairs[ugmu].length)];
+    return [ugmu, ypmu]
+}
+
+function getRandomPairWithUgmu(ugmu) {
+    if (!checkUgmu(ugmu)) {
+        return ''
+    }
+    var ypmu = allowPairs[ugmu][Math.floor(Math.random() * allowPairs[ugmu].length)];
+    return [ugmu, ypmu]
+}
+
+function getRandomPairWithUgmuId(ugmuId) {
+    if (!checkUgmuId(ugmuId)) {
+        return ''
+    }
+    var ugmu = allowUgmus[ugmuId];
+    var ypmu = allowPairs[ugmu][Math.floor(Math.random() * allowPairs[ugmu].length)];
+    return [ugmu, ypmu]
+}
+
+function getPairByYumuIdWithUgmu(ugmu, ypmuId) {
+    if (!checkUgmu(ugmu) || allowPairs[ugmu][ypmuId] === undefined) {
+        return ''
+    }
+    return [ugmu, allowPairs[ugmu][ypmuId]]
+}
+
+function getPairByIds(ugmuId, ypmuId) {
+    if (!checkUgmuId(ugmuId) || allowPairs[allowUgmus[ugmuId]][ypmuId] === undefined) {
+        return ''
+    }
+    return [allowUgmus[ugmuId], allowPairs[allowUgmus[ugmuId]][ypmuId]]
+}
+
+function checkUgmu(ugmu) {
+    return allowPairs[ugmu] !== undefined
+}
+
+function checkUgmuId(ugmuId) {
+    return allowUgmus[ugmuId] !== undefined
+}
+
+function checkPairs(pairs) {
+    return pairs !== undefined && pairs.length === 2;
+
+}
+
+function debug_showAllowPairs() {
+    var x = 0, y = 0;
+    while (true) {
+        if (getPairByIds(x, y) === '') {
+            if (y === 0) {
+                break;
+            } else {
+                x++;
+                y = 0;
+            }
+        } else {
+            console.log(getPairByIds(x, y));
+            y++;
+        }
+    }
+}
+
+function debug_showDictOfAllowPairsByDictId(dictId) {
+    var x = 0, y = 0;
+    while (true) {
+        if (getPairByIds(x, y) === '') {
+            if (y === 0) {
+                break;
+            } else {
+                x++;
+                y = 0;
+            }
+        } else {
+            var pairs = getPairByIds(x, y);
+            console.log(pairs.concat(getDictByPair(dictId, pairs)));
+            y++;
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,156 @@
+'use strict';
+const MODES = {
+    "SVJI": 0,
+    "UPXU": 1
+};
+var nowId = 0;
+var nowMode = MODES.SVJI;
+var actionFlag = true;
+var nowUgmu = 'sh';
+var nowYpmu = 'uang';
+var nowPair = [nowUgmu, nowYpmu];
+var nowDictUgmu = 'U';
+var nowDictYpmu = 'd';
+var nowDict = [nowDictUgmu, nowDictYpmu];
+var nowUgmuId = -1;
+var nowYpmuId = 0;
+window.onload = function () {
+    var dictSelect = document.getElementById('dictSelect');
+    for (var dictId in dictsList) {
+        var dictOption = document.createElement('option');
+        dictOption.innerHTML = dictsList[dictId];
+        dictSelect.appendChild(dictOption)
+    }
+    document.getElementById('a1').focus();
+};
+
+document.onkeydown = function (event) {
+    switch (event.keyCode) {
+        case 37:
+            document.getElementById('a1').focus();
+            break;
+        case 39:
+            document.getElementById('a2').focus();
+            break;
+        case 8:
+            if (document.getElementById('a2').value === '') {
+                redo()
+            }
+            break;
+        case 13: case 32:
+            if (check()) {
+                next()
+            } else {
+                redo()
+            }
+            break
+    }
+};
+
+window.onresize = function () {
+    document.body.style.height = window.innerHeight + 'px'
+};
+
+function doAction(x) {
+    x.value = x.value.replace(/[^a-zA-Z;]/g, '');
+    if (x.value.length === 1) {
+        if (x.id === 'a1') {
+            x.value = x.value.toUpperCase();
+            if (actionFlag) {
+                document.getElementById('a2').focus();
+                actionFlag = false
+            }
+        } else if (x.id === 'a2') {
+            x.value = x.value.toLowerCase()
+        }
+    }
+    check()
+}
+
+function check() {
+    if (document.getElementById('a1').value.length === 1 && document.getElementById('a2').value.length === 1) {
+        if (document.getElementById('a1').value === nowDictUgmu && document.getElementById('a2').value === nowDictYpmu) {
+            document.getElementById('btn_next').style.display = 'block';
+            document.getElementById('btn_redo').style.display = 'none';
+            return true
+        }
+    }
+    document.getElementById('btn_next').style.display = 'none';
+    document.getElementById('btn_redo').style.display = 'block';
+    if (document.getElementById('a1').value.length === 0) {
+        actionFlag = true
+    }
+    return false
+}
+
+function next() {
+    redo();
+    switch (nowMode) {
+        case(MODES.SVJI):
+            nowPair = getRandomPair();
+            nowDict = getDictByPair(nowId, nowPair);
+            nowUgmu = getCaptialUpperCase(nowPair[0]);
+            nowYpmu = nowPair[1];
+            nowDictUgmu = getCaptialUpperCase(nowDict[0]);
+            nowDictYpmu = nowDict[1];
+            document.getElementById('q1').innerHTML = nowUgmu;
+            document.getElementById('q2').innerHTML = nowYpmu;
+            break;
+        case (MODES.UPXU):
+            nowPair = getPairByIds(nowUgmuId, nowYpmuId);
+            if (nowPair === '') {
+                if (nowYpmuId === 0) {
+                    nowUgmuId = 0;
+                    nowYpmuId = 0
+                } else {
+                    nowUgmuId++;
+                    nowYpmuId = 0
+                }
+                next();
+                return
+            } else {
+                nowYpmuId++;
+            }
+            nowDict = getDictByPair(nowId, nowPair);
+            nowUgmu = getCaptialUpperCase(nowPair[0]);
+            nowYpmu = nowPair[1];
+            nowDictUgmu = getCaptialUpperCase(nowDict[0]);
+            nowDictYpmu = nowDict[1];
+            document.getElementById('q1').innerHTML = nowUgmu;
+            document.getElementById('q2').innerHTML = nowYpmu;
+            break
+    }
+}
+
+function redo() {
+    actionFlag = true;
+    document.getElementById('a1').value = '';
+    document.getElementById('a2').value = '';
+    document.getElementById('a1').focus();
+    document.getElementById('btn_next').style.display = 'none';
+    document.getElementById('btn_redo').style.display = 'block'
+}
+
+function changeDict(x) {
+    var id = dictsList.indexOf(x);
+    if (id === -1) {
+        alert('参数错误');
+        document.getElementById('dict').value = dictsList[0]
+    } else {
+        nowId = id;
+    }
+    next()
+}
+
+function changeMode(x) {
+    if (x === '随机') {
+        nowMode = MODES.SVJI
+    } else if (x === '顺序') {
+        nowMode = MODES.UPXU
+    }
+    next()
+}
+
+function getCaptialUpperCase(x) {
+    return x.substr(0, 1).toUpperCase().concat(x.substr(1, x.length).toLowerCase());
+}
