9ddb3923d95e355269b9870cc393f48d0907ac8a
BlueSky <admin@ihint.me>
2017-10-04 21:51:21 +0800

    v3.1: change to 1 input

--- js/view/core.js
+++ js/view/core.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/** last changed: 2017.10.4 */
+/** last changed: 2017.10.04 */
 
 // core
 var nowModel = model();
@@ -30,13 +30,11 @@ window.onload = function () {
 	nowModel.ypmu = 'uang';
 	nowModel.beReady(setting.schemeId);
 	/** view */
-	document.getElementById('a1').click();
+	document.getElementById('a').click();
 	if (setting.modeId === modes.getIdByName.WUPNYN) {
-		document.getElementById('q1').innerHTML = '';
-		document.getElementById('q2').innerHTML = '';
+		document.getElementById('q').innerHTML = '&nbsp';
 	} else {
-		document.getElementById('q1').innerHTML = nowModel.getUgmu();
-		document.getElementById('q2').innerHTML = nowModel.getYpmu();
+		document.getElementById('q').innerHTML = nowModel.getUgmu() + nowModel.getYpmu();
 	}
 	document.getElementById('example').innerHTML = nowModel.getExample();
 	/** other things */
@@ -45,26 +43,26 @@ window.onload = function () {
 
 // check
 var check = function () {
-	var a1 = document.getElementById('a1').value;
-	var a2 = document.getElementById('a2').value;
-	nowModel.setInputUgmu(a1);
-	nowModel.setInputYpmu(a2);
-	if (nowModel.check()) {
-		document.getElementById('btn_next').style.display = 'block';
-		document.getElementById('btn_redo').style.display = 'none';
-		return true;
-	} else {
-		document.getElementById('btn_next').style.display = 'none';
-		document.getElementById('btn_redo').style.display = 'block';
-		return false;
+	var inputUgmu = document.getElementById('a').value[0];
+	var inputYpmu = document.getElementById('a').value[1];
+	if (inputYpmu !== undefined) {
+		nowModel.setInputUgmu(inputUgmu);
+		nowModel.setInputYpmu(inputYpmu);
+		if (nowModel.check()) {
+			document.getElementById('btn_next').style.display = 'block';
+			document.getElementById('btn_redo').style.display = 'none';
+			return true;
+		}
 	}
+	document.getElementById('btn_next').style.display = 'none';
+	document.getElementById('btn_redo').style.display = 'block';
+	return false;
 };
 
 // redo
 var redo = function () {
-	document.getElementById('a1').value = '';
-	document.getElementById('a2').value = '';
-	document.getElementById('a1').click();
+	document.getElementById('a').value = '';
+	document.getElementById('a').click();
 	document.getElementById('btn_next').style.display = 'none';
 	document.getElementById('btn_redo').style.display = 'block';
 };
@@ -108,11 +106,9 @@ var next = function () {
 		newModel.beReady(setting.schemeId);
 		nowModel = newModel;
 		if (setting.modeId === modes.getIdByName.WUPNYN) {
-			document.getElementById('q1').innerHTML = '';
-			document.getElementById('q2').innerHTML = '';
+			document.getElementById('q').innerHTML = '&nbsp';
 		} else {
-			document.getElementById('q1').innerHTML = nowModel.getUgmu();
-			document.getElementById('q2').innerHTML = nowModel.getYpmu();
+			document.getElementById('q').innerHTML = nowModel.getUgmu() + nowModel.getYpmu();
 		}
 		document.getElementById('example').innerHTML = nowModel.getExample();
 	}
@@ -136,17 +132,6 @@ var switchTips = function (x) {
 // actions
 var keyAction = function (event) {
 	switch (event.keyCode) {
-		case 37: // <- Left
-			document.getElementById('a1').click();
-			break;
-		case 39: // -> Right
-			document.getElementById('a2').click();
-			break;
-		case 8: // Backspace
-			if (document.getElementById('a2').value === '') {
-				redo();
-			}
-			break;
 		case 13: // Enter
 		case 32: // Space
 			if (check()) {
@@ -158,26 +143,10 @@ var keyAction = function (event) {
 	}
 };
 
-var doAction = function (event, element) {
-	var keyCode = event.keyCode;
-	if (
-		(keyCode >= 48 && keyCode <= 57) ||
-		(keyCode >= 65 && keyCode <= 90) ||
-		(keyCode >= 96 && keyCode <= 105)
-	) {
-		event.preventDefault();
-		if (!element.value.length || element.value === ' ') {
-			var value = event.key.replace(/[^a-zA-Z;]/g, '');
-			if (value.length) {
-				if (element.id === 'a1') {
-					document.getElementById('a2').focus();
-					element.value = value.toUpperCase();
-				} else if (element.id === 'a2') {
-					element.value = value.toLowerCase();
-				}
-			}
-		}
-	}
+var doAction = function (x) {
+	x.value = x.value.substr(0, 2);
+	x.value = x.value.replace(/[^a-zA-Z;]/g, '');
+	x.value = x.value.substr(0, 1).toUpperCase().concat(x.value.substr(1, x.value.length).toLowerCase());
 	check();
 };
 
